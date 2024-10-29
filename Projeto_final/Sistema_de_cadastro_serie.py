8966642ea2ca3054db266a088e5b642a0656a61c
Edivaldo Sousa Pinheiro <161406590+EdivaldoSP@users.noreply.github.com>
2024-10-28 21:05:52 -0300

    Update Sistema_de_cadastro_serie.py

--- Projeto_final/Sistema_de_cadastro_serie.py
+++ Projeto_final/Sistema_de_cadastro_serie.py
@@ -1,11 +1,11 @@
 import time
 import os
 opcao = 0
-series_add = {
+series_add = { # VARIAVEL TEMPORARIA PARA ARMAZENAR SERIES ADICIONADAS
 
 }
 
-series = [{
+series = [{                 # LISTA DE DICIONARIOS (SERIES CADASTRADAS)
     'nome': 'Friends',
     'ano': 1994,
     'genero': 'comedia',
@@ -32,7 +32,7 @@ series = [{
     'descricao': 'Um grupo de sobreviventes que vive na periferia de uma cidade',
 }]
 
-while opcao != 7:
+while opcao != 7: # ENQUANTO MINHA VARIAVEL OPCAO FOR DIFERENTE DE 7, PRINTAR O MENU 
     print('{:=^30}'.format('| SEJA BEM VINDO (A) |'))
     print()
     print('{:-^31}'.format('> SISTEMA DE CADASTRO SÉRIE <'))
@@ -50,12 +50,14 @@ while opcao != 7:
     os.system("clear") # LIMPANDO O TERMINAL
 
     # menu de opções
-    try:
+    try: # O CÓDIGO IRÁ RODAR E QUALQUER TIPO DE ERRO IRÁ CAIR NO EXCEPT
+        
         opcao_int = int(opcao)
 
         # opção para  imprimir todas as séries
         if int(opcao_int) == 1:
-            try:
+            try: # O CÓDIGO IRÁ RODAR E QUALQUER TIPO DE ERRO IRÁ CAIR NO EXCEPT
+
                 for i in range(0, len(series)): # PERCORRER A LISTA DE SÉRIES E PRINTA CADA DICIONÁRIO.
                     print("-----" * 30)
                     print(f"{i+1}ª SÉRIE:\nNome: {series[i]['nome']} \nAno: {series[i]['ano']} \nGênero: {series[i]['genero']} \nDescrição: {series[i]['descricao']}\n")
@@ -77,9 +79,10 @@ while opcao != 7:
             except:
                 print('{:-^70}'.format("\n| ERRO AO IMPRIMIR A LISTA DE SÉRIES. |\n"))
 
-        # opção para imprimie um intervalo da das séries listadas
+        # opção para imprimir um intervalo da das séries listadas
         elif int(opcao_int) == 2:
-            try:
+            try: # O CÓDIGO IRÁ RODAR E QUALQUER TIPO DE ERRO IRÁ CAIR NO EXCEPT
+
                 inicio = int(input("Início do trecho a ser impresso: "))
                 fim = int(input("Fim do trecho a ser impresso: "))
                 if inicio >= 0  and fim <= len(series): # VERIFICA SE O INTERVALO É VÁLIDO
@@ -108,7 +111,8 @@ while opcao != 7:
 
         # opção para cadastrar uma nova série
         elif int(opcao_int) == 3:
-            try:
+            try: # O CÓDIGO IRÁ RODAR E QUALQUER TIPO DE ERRO IRÁ CAIR NO EXCEPT
+
                 if len(series) <=50: # VERIFICA SE O LIMITE DE SÉRIES CADASTRADAS ULTRAPASSOU
 
                     # ADICIONA UMA NOVA SÉRIE NA VARIÁVEL TEMPORARIA (series_add)
@@ -117,7 +121,7 @@ while opcao != 7:
                         series_add['ano'] = int(input("Digite o ano de lançamento: "))
                         series_add['genero'] = str(input("Digite o gênero: "))
                         series_add['descricao'] = str(input("Digite a descrição: "))
-                        series.append(series_add.copy())
+                        series.append(series_add.copy()) # ADICIONA AS SÉRIES A LISTA 
                     time.sleep(1)
                     print('{:-^98}'.format("\n| SÉRIE ADICIONADA COM SUCESSO |\n"))
                     
@@ -143,7 +147,8 @@ while opcao != 7:
 
         # opção para deletar uma série cadastrada sem usar break
         elif int(opcao_int) == 4:
-            try:
+            try: # O CÓDIGO IRÁ RODAR E QUALQUER TIPO DE ERRO IRÁ CAIR NO EXCEPT
+
                 encontrar_series_1 = False # VARIVAEL FLAG COMO FALSE
                 print("SÉRIES A SEREM DELETADAS:") 
                 for i in range(0, len(series)): # PRINT AS SÉRIES DENTRO DA LISTA
@@ -151,15 +156,15 @@ while opcao != 7:
 
                 deletar = input("DIGITE O NOME (CHAVE) DA SÉRIE PARA DELETAR: ")
                 i = 0
-                while i < len(series) and not encontrar_series_1:
-                    if (series[i]['nome']).lower() == deletar.lower():
+                while i < len(series) and not encontrar_series_1: # VERIFICA CADA INDICE (SERIE) NA LISTA  
+                    if (series[i]['nome']).lower() == deletar.lower(): # SE ENCONTRAR, ELE DELETA
                         encontrar_series_1 = True
                         del series[i]
                         print("APAGANDO...")
                         time.sleep(1) # DELAY DE 1 SEC.
                         print('{:-^98}'.format("\n| SÉRIE DELETADA COM SUCESSO! |\n"))
         
-                    else:
+                    else: # SENAO, ELE VAI ADICIONANDO + 1 A VARIAVEL "i" ATE PERCORRER TODOS OS ITENS NA LISTA
                         i += 1
                 if not encontrar_series_1:
                         print('{:-^136}'.format("\n| SÉRIE NÃO ENCONTRADA. TENTE NOVAMENTE... |\n"))
@@ -182,8 +187,9 @@ while opcao != 7:
 
         # opção para buscar uma série cadastrada sem usar o break
         elif int(opcao_int) == 5:
-            try:
-                encontrar_series_2 = False
+            try: # O CÓDIGO IRÁ RODAR E QUALQUER TIPO DE ERRO IRÁ CAIR NO EXCEPT
+
+                encontrar_series_2 = False # MESMA LÓGICA DA OPÇÃO ANTERIOR
                 buscar = str(input("DIGITE O NOME (CHAVE) PARA REALIZAR A BUSCA: "))
                 for i in range(0, len(series)):
                     if (series[i]['nome']).lower() == buscar.lower():
@@ -216,11 +222,12 @@ while opcao != 7:
 
         # opção para editar uma série sem usar break
         elif int(opcao_int) == 6:
-            try:
+            try: # O CÓDIGO IRÁ RODAR E QUALQUER TIPO DE ERRO IRÁ CAIR NO EXCEPT
+
                 encontrar_series_3 = False
                 mudar = str(input("DIGITE O NOME (CHAVE) PARA EDITAR A SÉRIE: "))
-                for i in range(0, len(series)):
-                    if (series[i]['nome']).lower() == mudar.lower():
+                for i in range(0, len(series)): # VAI PROCURAR A SÉRIE NA LISTA
+                    if (series[i]['nome']).lower() == mudar.lower(): # SE ENCONTRAR, ENTÃO VAI CONTINUAR O CÓD.
                         encontrar_series_3 = True
                         print("Buscando...")
                         time.sleep(1)
@@ -236,14 +243,20 @@ Opções:
                         alterar = input("O que deseja alterar? ")
                         if alterar == '1':
                             series[i]['nome'] = str(input("Digite o novo nome: "))
+                            print('{:-^98}'.format("\n| SÉRIE ATUALIZADA COM SUCESSO! |\n")) 
                         elif alterar == '2':
                             series[i]['ano'] = int(input("Digite a nova data de lançamento: "))
+                            print('{:-^98}'.format("\n| SÉRIE ATUALIZADA COM SUCESSO! |\n")) 
                         elif alterar == '3':
                             series[i]['genero'] = str(input("Digite o novo genero: "))
+                            print('{:-^98}'.format("\n| SÉRIE ATUALIZADA COM SUCESSO! |\n")) 
                         elif alterar == '4':
                             series[i]['descricao'] = str(input("Digite a nova descrição: "))
+                            print('{:-^98}'.format("\n| SÉRIE ATUALIZADA COM SUCESSO! |\n")) 
+                        else:
+                            print('{:-^110}'.format("\n| OPÇÃO INVÁLIDA. TENTE NOVAMENTE! |\n"))
                         time.sleep(1)
-                        print('{:-^98}'.format("\n| SÉRIE ATUALIZADA COM SUCESSO! |\n")) 
+                        
                 
                 if not encontrar_series_3:
                     os.system("clear")
@@ -261,7 +274,6 @@ Opções:
                     print('{:-^70}'.format("\n| ERRO. VOLTANDO PARA O MENU... |\n"))
                     time.sleep(1)
 
-                
             except:
                 print('{:-^70}'.format("\n| ERRO AO BUSCAR A SÉRIE. |\n"))
         
@@ -269,7 +281,7 @@ Opções:
         elif int(opcao_int) == 7:
             opcao = 7
 
-    except ValueError:
+    except:
         print('{:-^114}'.format("\n| OPÇÃO INVÁLIDA. TENTE NOVAMENTE! |\n"))
 print('{:-^70}'.format("\n| FINALIZANDO... |\n"))
 time.sleep(1)
